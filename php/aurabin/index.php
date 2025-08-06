ac4409d323e26f84e9d2a508b69865701de11e27
Márcio Simão <marciosimao0825@gmail.com>
2025-08-06 15:24:36 +0100

    arquivo index

--- php/aurabin/index.php
+++ php/aurabin/index.php
@@ -4,6 +4,8 @@
 
     print ' <br> texto usando print </br>'; 
     print " Texto usando print com aspas duplas";
+
+    include 'configuracao.php'
     //Comentário de uma linha 
     #comentário de uma linha 
     /* Comentário de multiplas linhs 
