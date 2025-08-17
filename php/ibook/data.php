eae0a084c025485980624c4fdfa623808b319e4c
Márcio Simão <marciosimao0825@gmail.com>
2025-08-17 21:56:43 +0100

    Create data.php

---
+++ php/ibook/data.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data</title>
+</head>
+<body>
+    <?php 
+        echo'Hoje é ' . date('d/m/Y');
+        
+        echo'<br> agora são ' . date('H:i:s');
+    ?>
+</body>
+</html>
