333a985659a66e0da567330ae993e7d4a4086eb7
Márcio Simão <marciosimao0825@gmail.com>
2025-08-17 22:15:03 +0100

    Update 001.php

--- php/ibook/cap-02/desafios/001/001.php
+++ php/ibook/cap-02/desafios/001/001.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php 
+        /*Questão: Nafunção date(), experimente mudar o Y para y. O que acontece? */
         echo'Hoje é ' . date('d/m/y');
         //percebi que usando o "y" minusculo aparece apenas os dois últimos digitos do ano 
         echo'<br> agora são ' . date('H:i:s');
