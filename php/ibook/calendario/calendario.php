ca9d488470fb409fba722b187d41c12d8544a60e
Márcio Simão <marciosimao0825@gmail.com>
2025-08-17 02:20:34 +0100

    calendário

---
+++ php/ibook/calendario/calendario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php 
+        echo "<h1>CALENDARIO</h1>";
+        echo "<hr>";
+    ?>
+    
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sab</th>
+        </tr>
+    </table>  
+</body>
+</html>
