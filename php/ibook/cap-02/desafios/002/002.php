9e950e8c0a8e7b596ae91be0af4d802af2371806
Márcio Simão <marciosimao0825@gmail.com>
2025-08-17 22:22:28 +0100

    Update 002.php

--- php/ibook/cap-02/desafios/002/002.php
+++ php/ibook/cap-02/desafios/002/002.php
@@ -6,6 +6,12 @@
     <title>Desafio 002 do Capitulo 2 </title>
 </head>
 <body>
-    
+    <?php
+        /*Questão: Vocêconsegue exibir a hora no formato de 12 horas, am e pm? */
+        echo'Hoje é ' . date('d/m/y');
+        //percebi que usando o "y" minusculo aparece apenas os dois últimos digitos do ano 
+        echo'<br> agora são ' . date('h:i:s');
+        //usando o "h" minusculo estamos a utilizar a hora no formato AM e PM
+    ?>
 </body>
 </html>
