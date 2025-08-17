902e78af6e710d265d7024647558a455cfda11b0
Márcio Simão <marciosimao0825@gmail.com>
2025-08-17 22:08:08 +0100

    organizando

---
+++ php/ibook/cap-02/desafios/001/001.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 001 do Capitulo 2 </title>
+</head>
+<body>
+    <?php 
+        echo'Hoje é ' . date('d/m/y');
+        //percebi que usando o "y" minusculo aparece apenas os dois últimos digitos do ano 
+        echo'<br> agora são ' . date('H:i:s');
+    ?>
+</body>
+</html>
