3be03b6af9885828a38a3d233d2246431f569bb6
Márcio Simão <marciosimao0825@gmail.com>
2025-08-21 19:50:58 +0100

    Update 004.php

--- php/ibook/cap-02/desafios/004/004.php
+++ php/ibook/cap-02/desafios/004/004.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desafio 004 do Capitulo 02<title>
+    <title>Desafio 004 do Capitulo 02</title>
 </head>
 <body>
     <?php
-        echo'dia da semana: ' . date('w');
+        echo'hoje é ' . date('w');
+        echo' da semana ';
 
+        $hoje = date('w');
 
+        $sabado = date('6');
+
+        $res_dia = $sabado - $hoje;
+
+        echo"<br> </br>faltam $res_dia dia para sábado!";
+       
     ?>
 </body>
 </html>
