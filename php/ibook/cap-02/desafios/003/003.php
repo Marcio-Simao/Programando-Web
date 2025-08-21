88ee0881e92c88e45d763c6f40f056a38586795a
Márcio Simão <marciosimao0825@gmail.com>
2025-08-21 19:32:22 +0100

    Desafios

---
+++ php/ibook/cap-02/desafios/003/003.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 003 do Capitulo 2</title>
+</head>
+<body>
+    <?php
+        //Desafio: Esevocê tivesse que exibir o dia da semana? Como faria?
+        echo" Hoje é " .  date('D');
+    ?>
+</body>
+</html>
