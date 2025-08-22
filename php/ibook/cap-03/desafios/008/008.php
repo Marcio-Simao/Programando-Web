c851897c067f7e68817fd6565c596e973413f7e3
Márcio Simão <marciosimao0825@gmail.com>
2025-08-22 12:12:20 +0100

    Update 008.php

--- php/ibook/cap-03/desafios/008/008.php
+++ php/ibook/cap-03/desafios/008/008.php
@@ -3,9 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Desafio 008 Capítulo 03</title>
 </head>
 <body>
     
+    <?php
+        function linha()
+        {
+
+        }
+
+        function calendario() 
+        {
+            
+        }
+    ?>
+
+    <table border="1">
+        <tr>
+            <td>Dom</td>
+            <td>Seg</td>
+            <td>Ter</td>
+            <td>Qua</td>
+            <td>Qui</td>
+            <td>Sex</td>
+            <td>Sáb</td>
+        </tr>       
+    </table>
 </body>
 </html>
