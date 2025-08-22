bfa12e5e6291b6277e6f873743d201091695d6ac
Márcio Simão <marciosimao0825@gmail.com>
2025-08-22 11:42:17 +0100

    Update 007.php

--- php/ibook/cap-03/desafios/007/007.php
+++ php/ibook/cap-03/desafios/007/007.php
@@ -10,24 +10,24 @@
     <?php
         function linha($semana)
         {
-            echo
-            "
-                <tr>
-                    <td>{$semana[0]}</td>
-                    <td>{$semana[1]}</td>
-                    <td>{$semana[2]}</td>
-                    <td>{$semana[3]}</td>
-                    <td>{$semana[4]}</td>
-                    <td>{$semana[5]}</td>
-                    <td>{$semana[6]}</td>
-                 </tr>
-            ";
+            echo "<tr>";
+            for ($i=0; $i <= 6 ; $i++)    
+            {
+                if (isset($semana[$i])) 
+                {
+                    echo"<td>{$semana[$i]}</td>";    
+                }
+                else
+                {
+                    echo"<td></td>";
+                }    
+            }                
+            echo"</tr>";
         }
 
     ?>
 
     <?php
-
         function calendario()
         {
             $dia = 1;
@@ -40,12 +40,10 @@
                     linha($semana);
                     $semana = array();
                 }
-
                 $dia++;
             }
             linha($semana);
         } 
-
     ?>
 
     <table border="1">
@@ -60,5 +58,6 @@
         </tr>
         <?php calendario(); ?>
     </table>
+
 </body>
 </html>
