f563c3a001a455c02a5b5479c390e4a70595e910
Márcio Simão <marciosimao0825@gmail.com>
2025-08-27 22:09:08 +0100

    HTML

---
+++ php/ibook/cao-04/tarefa.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciador de Tarefas</title>
+</head>
+<body>
+    <h1>Gerenciador de Tarefas</h1>
+
+    <form action="">
+        <fieldset>
+            <legend>Nova tarefa</legend>
+            <label>
+                Tarefa:
+                <input type="text" name="nome">
+            </label>
+            <input type="submit" value="cadastrar">
+        </fieldset>    
+    </form>
+    <?php
+        if (isset($_GET['nome']))
+        {
+            $lista_tarefas[] = $_GET['nome'];
+        }
+    ?>
+    <br>
+    <table>
+        <tr>
+            <th>Tarefas</th>
+        </tr>
+        <?php foreach ($lista_tarefas as $tarefa) : ?>
+        <tr>
+            <td><?php echo $tarefa ?></td>
+        </tr>    
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
