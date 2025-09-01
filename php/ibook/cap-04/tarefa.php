5485df3e0e8fd7222547c4e6358b94e1e4d67f49
Márcio Simão <marciosimao0825@gmail.com>
2025-09-01 10:43:00 +0100

    Update tarefa.php

--- php/ibook/cap-04/tarefa.php
+++ php/ibook/cap-04/tarefa.php
@@ -22,7 +22,12 @@
     <?php
         if (isset($_GET['nome']))
         {
-            $lista_tarefas[] = $_GET['nome'];
+           $_SESSION['$lista_tarefas'][] = $_GET['nome'];
+        }
+        $lista_tarefas = array();
+        if(isset($_SESSION['lista_tarefas']))
+        {
+            $lista_tarefas = $_SESSION['lista_tarefas'];
         }
     ?>
     <br>
