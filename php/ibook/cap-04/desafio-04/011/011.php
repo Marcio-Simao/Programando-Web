7b9839d3a8323f75d78f33afcfc550c456093e4a
Márcio Simão <marciosimao0825@gmail.com>
2025-09-01 11:29:52 +0100

    Update 011.php

---
+++ php/ibook/cap-04/desafio-04/011/011.php
@@ -0,0 +1,24 @@
+<?php SESSION_START();?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 011 Capítulo 04</title>
+</head>
+<body>
+    <!--  Desafio: Usando os mesmos conceitos que vimos até agora, monte uma lista de contatos na qual devem ser cadastrados o nome, o telefone e o e-mail de cada contato. -->
+    <h1>Lista de contactos</h1>
+    <form action="">
+        <fieldset>
+            <legend>Lista de contactos<legend>  
+                <label>
+                    Nome: 
+                    <input type="text" name="nome">
+                </label>  
+
+                <input type="submit" value="Cadastrar">
+        </fieldset>  
+    </form>
+</body>
+</html>
