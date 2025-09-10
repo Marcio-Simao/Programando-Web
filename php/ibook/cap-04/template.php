dcf529d1683912fed3163a982331438de10d7589
Márcio Simão <marciosimao0825@gmail.com>
2025-09-10 10:14:16 +0100

    Update template.php

--- php/ibook/cap-04/template.php
+++ php/ibook/cap-04/template.php
@@ -28,32 +28,6 @@
         </fieldset>    
     </form>
 
-    <?php
-
-        session_start();
-        /* Inicia a sessão para armazenar dados entre diferentes requisições do usuário */
-
-        // Se foi enviado um valor pelo formulário (via GET)
-        if (isset($_GET['nome']))
-        {
-           // Armazena a nova tarefa dentro da sessão
-           $_SESSION['lista_tarefas'][] = $_GET['nome'];
-        }
-
-        // Se já existe uma lista de tarefas na sessão, carrega para a variável
-        if(isset($_SESSION['lista_tarefas']))
-        {
-            $lista_tarefas = $_SESSION['lista_tarefas'];
-        }
-        else
-        {
-            // Cria um array vazio para as tarefas
-            $lista_tarefas = array();
-        }
-
-        include "template.php"
-    ?>
-
     <br>
 
     <!-- Tabela para exibir as tarefas cadastradas -->
