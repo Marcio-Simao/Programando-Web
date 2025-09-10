88046aaae77d17afd38f12450608ffee461a6cfc
Márcio Simão <marciosimao0825@gmail.com>
2025-09-10 10:25:12 +0100

    organizando os capítulos

---
+++ php/ibook/cap-05/tarefa.php
@@ -0,0 +1,25 @@
+<?php
+
+    session_start();
+    /* Inicia a sessão para armazenar dados entre diferentes requisições do usuário */
+
+    // Se foi enviado um valor pelo formulário (via GET)
+    if (isset($_GET['nome']))
+    {
+       // Armazena a nova tarefa dentro da sessão
+       $_SESSION['lista_tarefas'][] = $_GET['nome'];
+    }
+
+    // Se já existe uma lista de tarefas na sessão, carrega para a variável
+    if(isset($_SESSION['lista_tarefas']))
+    {
+        $lista_tarefas = $_SESSION['lista_tarefas'];
+    }
+    else
+    {
+        // Cria um array vazio para as tarefas
+        $lista_tarefas = array();
+    }
+
+    include "template.php"
+?>
